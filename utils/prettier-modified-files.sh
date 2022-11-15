3396ffad323b7e866f558087ebe5bf47779f8d6c
Rusu Andrei-Cristian <rusuandreicristian32@gmail.com>
2022-11-15 09:31:20 +0200

    feat(utils): update prettier-modified-files command

--- utils/prettier-modified-files.sh
+++ utils/prettier-modified-files.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-modified_files=`git ls-files --other --modified --exclude-standard`
+modified_files=`git ls-files ../microservices/ --other --modified --exclude-standard`
 
 for file_path in $modified_files
 do
